d98ebbd1899b1f5337b876a4db46b4a32c65023b
Renzou1 <114614401+Renzou1@users.noreply.github.com>
2023-09-13 17:33:38 -0300

    drawing

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use ggez::*;
+use ggez::graphics::*;
+use ggez::glam::*;
 
 struct Current(u32, u32);
 
@@ -16,38 +18,54 @@ enum Direction{
 struct Head(Position, Direction);
 
 struct Snake{
-    head: Head;
-    body: Vec<Position>;
+    head: Head,
+    body: Vec<Position>,
 }
 
 
+
+
+fn main() {
+    let config = conf::Conf::new();
+    let (ctx, event_loop) = ContextBuilder::new("demo_ggez", "Renzo")
+        .default_conf(config)
+        .build()
+        .unwrap();
+
+    let state = State {
+        square: graphics::Mesh::new_rectangle(
+            &ctx,
+            graphics::DrawMode::fill(),
+            graphics::Rect::new(0.0,0.0,20.0,20.0),
+            Color::WHITE,
+        ).unwrap(),
+    };
+
+    println!("Hello, world!");
+    event::run(ctx, event_loop, state);
+}
+
 struct State {
-    dt: std::time::Duration,
+    square: graphics::Mesh,
 }
 
 impl ggez::event::EventHandler<GameError> for State {
-  fn update(&mut self, ctx: &mut Context) -> GameResult { //Context = ggez access to player input
+  fn update(&mut self, ctx: &mut Context) -> GameResult {
+        //const FPS: u32 = 60;
+        //while ctx.time.check_update_time(60){
+
+        //}
       return Ok(());
   }
   fn draw(&mut self, ctx: &mut Context) -> GameResult {
+        let mut canvas =
+            graphics::Canvas::from_frame(ctx, graphics::Color::BLUE);
+
+        canvas.draw(&self.square, Vec2::new(0.0,0.0));
+
+        canvas.finish(ctx)?;
       return Ok(());
   }
 }
 
-fn main(){
 
-    let state = State {
-        dt: std::time::Duration::new(0, 0),
-    };
-    // state = data that define the state of the game 
-
-    let c = conf::Conf::new();
-    //ctx = short for context
-
-    let (ctx, event_loop) = ContextBuilder::new("hello_ggez", "Renzo")
-    .default_conf(c)
-    .build()
-    .unwrap();
-
-    event::run(ctx, event_loop, state);
-}
