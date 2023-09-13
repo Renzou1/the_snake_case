f6cdfb3b35e5742daf7af7b811de61bad874c90b
Renzou1 <114614401+Renzou1@users.noreply.github.com>
2023-09-13 20:05:40 -0300

    speed increases

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,8 @@ use ggez::input::keyboard::{KeyCode, KeyMods, KeyInput};
 use rand::Rng;
 
 const DISTANCE: f32 = 20.0;
+const RANGE_X: i32 = 96;
+const RANGE_Y: i32 = 51;
 
 fn main() {
     let config = conf::Conf::new();
@@ -12,39 +14,47 @@ fn main() {
         .default_conf(config)
         .build()
         .unwrap();
-    let mut x: f32 = (rand::thread_rng().gen_range(0..20) * DISTANCE as i32) as f32;
-    let mut y: f32 = (rand::thread_rng().gen_range(0..20) * DISTANCE as i32) as f32;
+    let mut x: f32 = (rand::thread_rng().gen_range(0..RANGE_X) * DISTANCE as i32) as f32;
+    let mut y: f32 = (rand::thread_rng().gen_range(0..RANGE_Y) * DISTANCE as i32) as f32;
     let state = State {
-        apple: (Vec2::new(x , y), graphics::Mesh::new_rectangle(
-            &ctx,
-            graphics::DrawMode::fill(),
-            graphics::Rect::new(0.0,0.0,20.0,20.0),
-            Color::RED,
-        ).unwrap(),),
-        head: (Vec2::new(0.0, 0.0), graphics::Mesh::new_rectangle(
-            &ctx,
-            graphics::DrawMode::fill(),
-            graphics::Rect::new(0.0,0.0,20.0,20.0),
-            Color::GREEN,
-        ).unwrap(), Direction::Down),
+        apple: (Vec2::new(x , y), 
+                graphics::Mesh::new_rectangle(
+                    &ctx,
+                    graphics::DrawMode::fill(),
+                    graphics::Rect::new(0.0,0.0,20.0,20.0),
+                    Color::RED,
+                    ).unwrap(),
+                ),
+
+        head: (Vec2::new(0.0, 0.0), 
+                graphics::Mesh::new_rectangle(
+                    &ctx,
+                    graphics::DrawMode::fill(),
+                    graphics::Rect::new(0.0,0.0,20.0,20.0),
+                    Color::GREEN,
+                ).unwrap(), 
+                Direction::NotSet),
+
+        speed: 20,
     };
     event::run(ctx, event_loop, state);
 }
 
 impl ggez::event::EventHandler<GameError> for State {
     fn update(&mut self, ctx: &mut Context) -> GameResult {
-            let speed: u32 = 3;
-
-            while ctx.time.check_update_time(speed){
+            while ctx.time.check_update_time(self.speed){
                 match self.head.2 {
                     Direction::Up => self.head.0.y = self.head.0.y - DISTANCE,
                     Direction::Down => self.head.0.y = self.head.0.y + DISTANCE,                
                     Direction::Right => self.head.0.x = self.head.0.x + DISTANCE,
                     Direction::Left => self.head.0.x = self.head.0.x - DISTANCE,
+                    Direction::NotSet => self.head.0.x = self.head.0.x,
                 }
                 if self.apple.0 == self.head.0
                 {
-                    self.head.2 = Direction::Right;
+                    self.apple.0.x = (rand::thread_rng().gen_range(0..RANGE_X) * DISTANCE as i32) as f32;
+                    self.apple.0.y = (rand::thread_rng().gen_range(0..RANGE_Y) * DISTANCE as i32) as f32;
+                    self.speed = (self.speed as f32 * 1.1) as u32 ;
                 }
             }
         return Ok(());
@@ -87,6 +97,7 @@ impl ggez::event::EventHandler<GameError> for State {
 struct State {
     apple: (Vec2, graphics::Mesh),
     head: (Vec2, graphics::Mesh, Direction),
+    speed: u32,
     //head: (Position, Direction, graphics::Mesh)
 }
 
@@ -98,6 +109,7 @@ struct Snake{
 struct PositionInfo(Vec2, Vec2);
 
 enum Direction{
+    NotSet,
     Up,
     Down,    
     Right,
